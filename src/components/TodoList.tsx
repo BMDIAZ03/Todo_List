2662526e273302c981ca887939b47941af1a0b08
BrayanMejia03 <95449259+BrayanMejia03@users.noreply.github.com>
2024-11-20 16:00:06 -0400

    style(todo-list): add styling for TodoList component
    Added styling for todo-list unordered list. Includes list-style reset, margin and padding reset, and removal of bottom border for the last list item.

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,22 +1,34 @@
 import { useState } from 'react';
 import TodoItem from './TodoItem';
 import AddTodo from './AddTodo';
+import '../styles/TodoList.css';
 import React from 'react';
 
-const TodoList: React.FC = () => {
-  const [todos, setTodos] = useState<string[]>([]);
+interface Todo {
+  text: string;
+  completed: boolean;
+}
 
-  const handleAddTodo = (todo: string) => {
-    setTodos([...todos, todo]);
+const TodoList: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAddTodo = (text: string) => {
+    setTodos([...todos, { text, completed: false }]);
   };
 
   const handleDeleteTodo = (index: number) => {
     setTodos(todos.filter((_, i) => i !== index));
   };
 
-  const handleEditTodo = (index: number, newTodo: string) => {
+  const handleEditTodo = (index: number, newText: string) => {
     const updatedTodos = [...todos];
-    updatedTodos[index] = newTodo;
+    updatedTodos[index].text = newText;
+    setTodos(updatedTodos);
+  };
+
+  const handleToggleTodo = (index: number) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].completed = !updatedTodos[index].completed;
     setTodos(updatedTodos);
   };
 
@@ -27,9 +39,11 @@ const TodoList: React.FC = () => {
         {todos.map((todo, index) => (
           <TodoItem
             key={index}
-            todo={todo}
+            todo={todo.text}
+            completed={todo.completed}
             onDelete={() => handleDeleteTodo(index)}
-            onEdit={(newTodo) => handleEditTodo(index, newTodo)}
+            onEdit={(newText) => handleEditTodo(index, newText)}
+            onToggle={() => handleToggleTodo(index)}
           />
         ))}
       </ul>

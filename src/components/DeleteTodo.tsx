3782c2909ce523633dd1731adc12f30ddd6197a5
BrayanMejia03 <95449259+BrayanMejia03@users.noreply.github.com>
2024-11-20 15:56:50 -0400

    style(delete-todo): add button styling for DeleteTodo component
    Added styling for delete-todo button including padding, background color, border, and hover effect.

--- src/components/DeleteTodo.tsx
+++ src/components/DeleteTodo.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "../styles/DeleteTodo.css"
 
 interface DeleteTodoProps {
     onDelete: () => void;
   }
   
   const DeleteTodo: React.FC<DeleteTodoProps> = ({ onDelete }) => {
-    return <button onClick={onDelete}>Eliminar</button>;
+    return <button onClick={onDelete} className="delete-todo" >Eliminar</button>;
   };
   
   export default DeleteTodo;

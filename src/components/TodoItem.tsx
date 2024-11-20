5ff566e488ee8efc916678eb59eeec2003c917fd
BrayanMejia03 <95449259+BrayanMejia03@users.noreply.github.com>
2024-11-20 15:59:21 -0400

    style(todo-item): add styling for TodoItem component
    Added styling for todo-item, todo-content, and todo-actions. Includes layout, padding, text alignment, checkbox cursor, and completed state styles.

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import DeleteTodo from './DeleteTodo';
 import EditTodo from './EditTodo';
+import "../styles/TodoItem.css";
 
 interface TodoItemProps {
     todo: string;
+    completed: boolean;
     onDelete: () => void;
     onEdit: (newTodo: string) => void;
+    onToggle: () => void;
   }
   
-  const TodoItem: React.FC<TodoItemProps> = ({ todo, onDelete, onEdit }) => {
+  const TodoItem: React.FC<TodoItemProps> = ({ todo, completed, onDelete, onEdit ,onToggle}) => {
     return (
-      <li className="todo-item">
-        <span>{todo}</span>
-        <EditTodo todo={todo} onEdit={onEdit} />
-        <DeleteTodo onDelete={onDelete} />
+      <li className={`todo-item ${completed ? 'completed' : ''}`}>
+        <div className="todo-content"> 
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={onToggle}
+        />
+            <span>{todo}</span>
+        </div>
+        <div className="todo-actions">
+          <EditTodo todo={todo} onEdit={onEdit} />
+          <DeleteTodo onDelete={onDelete} />
+        </div>
       </li>
     );
   };

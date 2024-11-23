df11b63f1c3e93e50fb53f89e6b23e091ffb5bd1
BrayanMejia03 <95449259+BrayanMejia03@users.noreply.github.com>
2024-11-22 20:56:34 -0400

    feat: add database connection setup using node-adodb and dotenv
    - Imported ADODB from "node-adodb" for Access database connection.
    - Imported dotenv to handle environment variables.
    - Configured environment variables for the database path.
    - Created database connection using ADODB and environment variables.
    - Exported the database connection for use in other modules.

--- src/server/database/db.js
+++ src/server/database/db.js
@@ -1,13 +1,12 @@
 import ADODB from "node-adodb";
 import dotenv from "dotenv";
 
-// Configuración de variables de entorno
+
 dotenv.config();
 
-// Obtener la ruta de la base de datos desde las variables de entorno
-const DB_PATH = dotenv.config().parsed?.DB_PATH || './database/todolist.accdb';
 
-// Crear la conexión
+const DB_PATH = dotenv.config().parsed?.DB_PATH || './database/todolist.accdb'
+
 const connection = ADODB.open(
   `Provider=Microsoft.ACE.OLEDB.12.0;Data Source=${DB_PATH};Persist Security Info=False;`
 );
